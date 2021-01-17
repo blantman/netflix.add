a162d5f52d8ce9fd61c131cbdd19a8f21406de68
Sergri <bizon.kiriloff2015@gmail.com>
2021-01-17 12:55:14 +0200

    master

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Button from "./components/Button";
+
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      count:0
+    }
+  }
+
+incrementCount = () => {
+  this.setState({
+    count: this.state.count + 1
+  })
+}
+
+decrementCount = () => {
+  this.setState({
+    count: this.state.count -1
+})
+}
+
+  render () {
+    let { count } = this.state;
+    return(
+      <div>
+        <h2>Count: { count }</h2>
+        <Button titile = "+" task = { () => this.incrementCount() } />
+        <Button titile = "-" task = { () => this.decrementCount() }/>
+      </div>
+    )
+  }
+}
